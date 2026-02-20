f065f9d42ac231ce5a047739d5cf69bd63df5531
Aravind Raghu <arraghu93@gmail.com>
2026-02-19 22:34:44 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // e.g. if your repo is github.com/aravindr-res01/my-portfolio, use '/my-portfolio/'
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/',
 })
