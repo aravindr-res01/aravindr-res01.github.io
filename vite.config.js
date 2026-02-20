fc61ea78fcd05829253daa381cc9e1cb724c9a29
Aravind Raghu <arraghu93@gmail.com>
2026-02-19 22:21:00 -0600

    added new files

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// IMPORTANT: Change 'portfolio' below to your GitHub repo name
+// e.g. if your repo is github.com/aravindr-res01/my-portfolio, use '/my-portfolio/'
+export default defineConfig({
+  plugins: [react()],
+  base: '/portfolio/',
+})
